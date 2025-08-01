65e67b04c997f3f15a9f489b20eadd1c19af12b2
odavi <alvesdelima.davi@gmail.com>
2025-07-31 23:29:11 -0300

    first commit

---
+++ gateway/src/main/java/com/dvvz/omdb/OmdbRestApplication.java
@@ -0,0 +1,13 @@
+package com.dvvz.omdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmdbRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmdbRestApplication.class, args);
+	}
+
+}
---
+++ gateway/src/test/java/com/dvvz/omdb/OmdbRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dvvz.omdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmdbRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
